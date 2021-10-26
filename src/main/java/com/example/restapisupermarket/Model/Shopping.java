e39008165c5adab82eae194faa8eceaa321a1a3c
Laercio Alberto A. de Ávila <92470064+laercioAAAvila@users.noreply.github.com>
2021-10-25 21:26:02 -0300

    Create Shopping.java
    Class model

---
+++ src/main/java/com/example/restapisupermarket/Model/Shopping.java
@@ -0,0 +1,25 @@
+package com.example.restapisupermarket.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Shopping {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String product;
+    private double unitWeight;
+    private double priceForUnitWeight;
+    private double priceOfPurchase;
+}
